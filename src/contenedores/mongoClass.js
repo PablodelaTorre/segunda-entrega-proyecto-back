776085388f68b9fbf859bd73c9018843128a7108
Pablo <delatorre.pablodaniel@gmail.com>
2022-06-22 18:48:04 -0300

    mongodbproducts funcionando todo

--- src/contenedores/mongoClass.js
+++ src/contenedores/mongoClass.js
@@ -17,10 +17,38 @@ class MongoClass{
         }
     }
 
+    async findById(id){
+        try {
+            const producto = await this.collection.findOne({_id:id})
+            return producto
+        } catch (error) {
+            throw new Error(`Error: ${error}`)
+        }
+    }
+
     async create(obj){
         try {
             const newProduct = await this.collection.create(obj)
             return newProduct
+        } catch (error) {
+            throw new Error('error: ',error)
+        }
+    }
+
+    async actualizarP(id,product){
+        try {
+            const deleteProduct = await this.collection.deleteOne({_id:id})
+            const newProduct = await this.collection.create(product)
+            return newProduct                
+        } catch (error) {
+            throw new Error(`Error: ${error}`)
+        }
+    }
+
+    async deleteById(id){
+        try {
+            const deleteProduct = await this.collection.deleteOne({_id:id})
+            return deleteProduct
         } catch (error) {
             throw new Error('error: ',error)
         }
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -12,6 +12,19 @@ router.get('/',async (req,res)=>{
     }
 })
 
+router.get('/:id', async(req,res)=>{
+    const {id} = req.params
+    const product = await api.findById(id)
+    res.json(product)
+})
+
+router.put('/:id', async(req,res)=>{
+    const {id} = req.params
+    const newProduct = req.body
+    const deleteProduct = await api.actualizarP(id,newProduct)
+    res.json(deleteProduct)
+})
+
 
 router.post('/',async (req,res)=>{
     try {
@@ -20,6 +33,12 @@ router.post('/',async (req,res)=>{
     } catch (error) {
         console.log(error)
     }
+})
+
+router.delete('/:id', async(req,res)=>{
+    const {id} = req.params
+    const deleteProduct = await api.deleteById(id)
+    res.json(deleteProduct)
 })
 
 export default router
